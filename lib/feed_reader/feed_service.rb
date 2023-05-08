09770bcf95ad2e106ca5094ea8a3978f134986f9
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-05-07 21:24:02 -0400

    Update feed_service.rb

--- lib/feed_reader/feed_service.rb
+++ lib/feed_reader/feed_service.rb
@@ -1,43 +1,46 @@
 require 'rss'
 require 'open-uri'
 
+module Feed
+    class Feed 
+        
+        def initialize(url)
+            @url = url
+        end    
 
-class Feed 
-  
-    
-    def initialize(url)
-        @url = url
+        def get_title
+            URI.open(@url) do |rss|
+                feed = RSS::Parser.parse(rss)
+                @title = feed.channel.title
+                return @title
+            end
+        end
+        
+        def get_items 
+            URI.open(@url) do |rss|
+                feed = RSS::Parser.parse(rss)
+                list_items = []
+                feed.items.each do |item|
+                    list_items << item.title
+                end
+                return list_items
+                
+            end
+        end
+        
+        def get_links
+            URI.open(@url) do |rss|
+                feed = RSS::Parser.parse(rss)
+                array_links = []
+                feed.items.each do |item|
+                    title = item.title
+                    link = item.link
+                    array_links << {title:title, link: link}
+                    
+                end
+                return array_links
+            end
+        end     
+
     end    
-
-    def get_title
-        URI.open(@url) do |rss|
-            feed = RSS::Parser.parse(rss)
-            @title = feed.channel.title
-            return @title
-        end
-    end
-    
-    def get_items 
-        URI.open(@url) do |rss|
-            feed = RSS::Parser.parse(rss)
-            feed.items.each do |item|
-                @items = item.title
-                return @items
-            end
-        end
-    end
-    
-    def get_links
-        URI.open(@url) do |rss|
-            feed = RSS::Parser.parse(rss)
-            feed.items.each do |item|
-                @links = item.link
-                return @links
-            end
-        end
-    end     
-
-end    
-
-feed = Feed.new("https://carlosmv.hashnode.dev/rss.xml")
-puts " #{feed.get_items}"
+end
