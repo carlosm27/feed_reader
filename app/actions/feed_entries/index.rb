958fd257014dd8eda9feae5789fa29ee157c7ff7
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-05-13 21:15:57 -0400

    Update index.rb

--- app/actions/feed_entries/index.rb
+++ app/actions/feed_entries/index.rb
@@ -7,36 +7,36 @@ module FeedReader
         include Feed
         include Deps["persistence.rom"]
 
+        def my_feed 
+          feed = rom.relations[:feeds]
+            .select(:rss)
+            .to_a
+          
+          array_entries = []  
+
+          feed.each do |element|
+            rss = element[:rss].to_s
+            
+            entry = Feed.new(rss)
+          
+            entries = entry.get_links
+            
+            array_entries << entries
+            
+          end   
+            
+          
+              
+          array_entries
+          
+    
+        end   
+
         def handle(*, response)
-          
-          
-
-            def my_feed 
-              feed = rom.relations[:feeds]
-                .select(:rss)
-                .to_a
-
-              hash = feed[0]
-              
-              rss = hash[:rss].to_s
-               
-              entry = Feed.new(rss)
-              
-              entries = entry.get_links
-              
-               [{
-                
-                "entries": entries
-              }]
-              
-            end   
+     
           entries = my_feed  
           response.format = :json
           response.body = my_feed.to_json
-
-            
-
-
         end
       end
     end
