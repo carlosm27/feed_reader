4bb26d996f4bcc688801f6b14e349f58e13d2b27
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-05-13 21:16:03 -0400

    Update app.rb

--- config/app.rb
+++ config/app.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
 require "hanami"
+require "rack/cors"
 
 module FeedReader
   class App < Hanami::App
     config.middleware.use :body_parser, :json
+    
+    config.middleware.use Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+      end
+    end
   end
 end
